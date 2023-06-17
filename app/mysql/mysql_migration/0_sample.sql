121dfa84fa3aac8743c48beae13dd6c8093a2329
Susumu Yagi <susumu.yagi@gmail.com>
2023-06-17 17:46:53 +0900

    インデックス付与(department_role_member.role_id) #8

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -4,5 +4,6 @@ ALTER TABLE session ADD INDEX linked_user_id_idx (linked_user_id);
 ALTER TABLE session ADD INDEX session_id_idx (session_id);
 
 ALTER TABLE department_role_member ADD INDEX user_id_idx (user_id);
+ALTER TABLE department_role_member ADD INDEX role_id_idx (role_id);
 
 ALTER TABLE match_group_member ADD INDEX user_id_idx (user_id);
