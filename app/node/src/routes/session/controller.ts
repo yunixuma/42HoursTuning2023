07e9cb0a9c64e97bed9397c4b969232d75ccc177
Susumu Yagi <susumu.yagi@gmail.com>
2023-06-17 09:22:49 +0900

    ログイン時のsha256の計算を効率化 #5

--- app/node/src/routes/session/controller.ts
+++ app/node/src/routes/session/controller.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { execSync } from "child_process";
+import crypto from 'crypto';
 import { v4 as uuidv4 } from "uuid";
 import { getUserIdByMailAndPassword } from "../users/repository";
 import {
@@ -33,12 +33,7 @@ sessionRouter.post(
     }
 
     const { mail, password }: { mail: string; password: string } = req.body;
-
-    const hashPassword = execSync(
-      `echo -n ${password} | shasum -a 256 | awk '{printf $1}'`,
-      { shell: "/bin/bash" }
-    ).toString();
-
+    const hashPassword = crypto.createHash('sha256').update(password).digest('hex')
     try {
       const userId = await getUserIdByMailAndPassword(mail, hashPassword);
       if (!userId) {
