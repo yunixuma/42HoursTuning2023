4c5c511e884b466040bbcdc39644a7e443da66aa
Susumu Yagi <susumu.yagi@gmail.com>
2023-06-18 13:03:31 +0900

    部署、オフィス指定時のマッチング修正

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,4 +1,5 @@
 ALTER TABLE user ADD INDEX mail_password_idx(mail, password);
+ALTER TABLE user ADD INDEX office_id_idx(office_id);
 ALTER TABLE user ADD COLUMN id INT NOT NULL AUTO_INCREMENT UNIQUE FIRST;
 
 ALTER TABLE session ADD INDEX linked_user_id_idx (linked_user_id);
@@ -6,6 +7,7 @@ ALTER TABLE session ADD INDEX session_id_idx (session_id);
 
 ALTER TABLE department_role_member ADD INDEX user_id_idx (user_id);
 ALTER TABLE department_role_member ADD INDEX role_id_idx (role_id);
+ALTER TABLE department_role_member ADD INDEX department_id_idx (department_id);
 
 ALTER TABLE match_group_member ADD INDEX user_id_idx (user_id);
 
